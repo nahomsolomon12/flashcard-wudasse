3d11a23cb684452e3cae069c487a283184f25684
nahomsolomon12 <nahomsolomon6@gmail.com>
2025-10-03 09:45:21 -0500

    backgroundimage update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
 
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-    </div>
+    <>
+      <div className='front-card'>
+        Orthodoxy - Etymology
+      </div>
+      <div className='back-card'>Ortho-Proper/Straight/Orderly; Doxa-Worship/Exaltation/Valuation</div>
+      <button className='next'>
+        Next
+      </button>
+    </>
 
   )
 }
